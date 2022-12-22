46e23403ebf1fb866e9125c43e04856f3857b901
anmolko <anmol@canosoft.com.np>
2022-12-22 15:49:00 +0545

    contact us update in frontend

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -7,106 +7,188 @@
         margin-top: 20px;
         text-align: center;
     }
+    .featured-desc a{
+        color:  #7a8a9e;
+    }
+    .featured-desc a:hover{
+        color:  #27aae1;
+    }
+
 </style>
 @endsection
 @section('content')
-        <!--Page Header Start-->
-        <section class="page-header" style="background-image: url({{asset('assets/frontend/images/backgrounds/page-header-bg.jpg')}});">
-            <div class="page-header-shape-1"></div>
-            <div class="page-header-shape-2"></div>
-            <div class="container">
-                <div class="page-header__inner">
-                    <ul class="thm-breadcrumb list-unstyled">
-                        <li><a href="/">Home</a></li>
-                        <li><span>.</span></li>
-                        <li><a href="{{url('/contact-us')}}">Contact </a></li>
-                    </ul>
-                    <h2>Contact Us</h2>
-                </div>
-            </div>
-        </section>
-        <!--Page Header End-->
-
-        <!--Contact Page Start-->
-        <section class="contact-page">
-            <div class="container">
-                <div class="section-title text-center">
-                    <span class="section-title__tagline">Contact with us</span>
-                    <h2 class="section-title__title">
-
-                        <span>Write us a </span>
-                        <span class="text-last">message</span>
-                    </h2>
-                </div>
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="contact-page__form">
-                            <form action="{{route('contact.store')}}" id="contactForm" class="comment-one__form contact-form-validated"
-                                novalidate="novalidate">
-                            @csrf
-
-                                <div class="row">
-                                    <div class="col-xl-6">
-                                        <div class="comment-form__input-box">
-                                            <input type="text" placeholder="Your name" name="name">
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6">
-                                        <div class="comment-form__input-box">
-                                            <input type="email" placeholder="Email Address" name="email">
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6">
-                                        <div class="comment-form__input-box">
-                                            <input type="text" placeholder="Phone number" name="phone">
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6">
-                                        <div class="comment-form__input-box">
-                                            <select name="subject" id="subject" class="form-select">
-                                                <option value="Trade Inquiry" >Trade Inquiry</option>
-                                                <option value="Ask a question" selected>Ask a question</option>
-                                                <option value="Share Feedback">Share Feedback</option>
-                                                <option value="Career Enquiry">Career Enquiry</option>
-                                                <option value="Demand Enquiry">Demand Enquiry</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-xl-12">
-                                        <div class="comment-form__input-box">
-                                            <textarea name="message" placeholder="Write Message"></textarea>
-                                        </div>
-                                        <button type="submit" class="thm-btn faqs-contact__btn"><span>Send a
-                                                message</span></button>
-                                        <div id="msgSubmit" class="hidden"></div>
-
-                                    </div>
-                                </div>
-                            </form>
+        <!-- page-title -->
+        <div class="ttm-page-title-row">
+            <div class="ttm-page-title-row-inner ttm-bgcolor-darkgrey">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-12">
+                            <div class="page-title-heading">
+                                <h2 class="title">Contact Us</h2>
+                            </div>
+                            <div class="breadcrumb-wrapper">
+                                <span>
+                                    <i class="ti ti-home margin_right1"></i>
+                                    <a title="Homepage" href="/">Home</a>
+                                </span>
+                                <span>Contact Us</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-        <!--Contact Page End-->
+        </div>
+        <!-- page-title end -->
+
+
+        <!--site-main start-->
+        <div class="site-main">
+
+
+            <!--- conatact-section -->
+            <section class="ttm-row conatact-section clearfix">
+                <div class="container">
+                    <!-- row -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <!-- section title -->
+                            <div class="section-title without-seperator title-style-center_text">
+                                <div class="title-header">
+                                    <h3>Reach out</h3>
+                                        <h2 class="title">Send Us a Message</h2>
+                                </div>
+                                <div class="title-desc">
+                                    <p>Have something in mind? Get in touch with us and we will get back to you as soon as possible. We are fully committed to apply full range of talent to creating perfect solution for each client’s needs.</p>
+                                </div>
+                            </div><!-- section title end -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="padding_right30 res-991-padding_right0 res-991-margin_bottom30">
+                                <div class="ttm_single_image-wrapper margin_bottom40">
+                                    <img class="img-fluid w-100" src="{{asset('images/office.jpeg')}}" alt="single-12">
+                                </div>
+                                <!--featured-icon-box-->
+                                <div class="featured-icon-box icon-align-before-content style11">
+                                    <div class="featured-icon">
+                                        <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-skincolor ttm-icon_element-size-md ttm-icon_element-style-round">
+                                            <i class="fa fa-phone"></i>
+                                        </div>
+                                    </div>
+                                    <div class="featured-content">
+                                        <div class="featured-title">
+                                            <h3>Give us a call</h3>
+                                        </div>
+                                        <div class="featured-desc"><a href="tel:@if(!empty(@$setting_data->phone)) {{@$setting_data->phone}} @else +9771238798 @endif">@if(!empty(@$setting_data->phone)) {{@$setting_data->phone}} @else +9771238798 @endif</a></div>
+                                    </div>
+                                </div><!-- featured-icon-box end-->
+                                <div class="ttm-horizontal_sep width-100 margin_top20 margin_bottom20"></div>
+                                <!--featured-icon-box-->
+                                <div class="featured-icon-box icon-align-before-content style11">
+                                    <div class="featured-icon">
+                                        <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-skincolor ttm-icon_element-size-md ttm-icon_element-style-round">
+                                            <i class="fa fa-globe"></i>
+                                        </div>
+                                    </div>
+                                    <div class="featured-content">
+                                        <div class="featured-title">
+                                            <h3>Send Email</h3>
+                                        </div>
+                                        <div class="featured-desc">
+                                            <a href="mailto:@if(!empty(@$setting_data->email)) {{@$setting_data->email}} @else example@gmail.com @endif">@if(!empty(@$setting_data->email)) {{@$setting_data->email}} @else example@gmail.com @endif</a>
+                                        </div>
+                                    </div>
+                                </div><!-- featured-icon-box end-->
+                                <div class="ttm-horizontal_sep width-100 margin_top20 margin_bottom20"></div>
+                                <!--featured-icon-box-->
+                                <div class="featured-icon-box icon-align-before-content style11 margin_bottom0">
+                                    <div class="featured-icon">
+                                        <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-skincolor ttm-icon_element-size-md ttm-icon_element-style-round">
+                                            <i class="ti ti-home"></i>
+                                        </div>
+                                    </div>
+                                    <div class="featured-content">
+                                        <div class="featured-title">
+                                            <h3>Visit Office</h3>
+                                        </div>
+                                        <div class="featured-desc">
+                                            {{@$setting_data->address}}
+                                        </div>
+                                    </div>
+                                </div><!-- featured-icon-box end-->
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="ttm-col-bgcolor-yes ttm-bg ttm-bgcolor-grey z-index-2 p-50 res-991-margin_top30 res-991-p-15">
+                                <div class="ttm-col-wrapper-bg-layer ttm-bg-layer"></div>
+                                <div class="layer-content">
+                                    <form id="contact_form" class="contact_form wrap-form clearfix" method="post" novalidate="novalidate" action="{{route('contact.store')}}">
+                                        @csrf
+                                        <div class="row ttm-boxes-spacing-15px">
+                                            <div class="col-md-12 ttm-box-col-wrapper">
+                                                <label>
+                                                    <span class="text-input margin_bottom0"><input name="name" type="text" value="" placeholder="Your Name" required="required"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-6 ttm-box-col-wrapper">
+                                                <label>
+                                                    <span class="text-input margin_bottom0"><input name="email" type="text" value="" placeholder="Your Email" required="required"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-6 ttm-box-col-wrapper">
+                                                <label>
+                                                    <span class="text-input margin_bottom0"><input name="subject" type="text" value="" placeholder="Subject" required="required"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-12 ttm-box-col-wrapper">
+                                                <label>
+                                                    <span class="text-input margin_bottom0"><input name="phone" type="text" value="" placeholder="Phone Number" required="required"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-12 ttm-box-col-wrapper">
+                                                <label>
+                                                    <span class="text-input margin_bottom0"><textarea name="message" rows="3" placeholder="Message" required="required"></textarea></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <button class="submit ttm-btn ttm-btn-size-lg ttm-btn-shape-square ttm-btn-style-fill ttm-btn-color-skincolor w-100" type="submit">SEND A MESSAGE!</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- row end -->
+                </div>
+            </section>
+            <!-- conatact-section end -->
 
         @if(@$setting_data->google_map)
 
-        <!--Contact Page Google Map Start-->
-        <section class="contact-page-google-map">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <iframe
-                            src="{{@$setting_data->google_map}}"
-                            class="contact-page-google-map__box" allowfullscreen></iframe>
+            <!--google_map-->
+            <div id="google_map" class="google_map">
+                <div class="map_container clearfix">
+                    <div id="map">
+                        <iframe src="{{@$setting_data->google_map}}" width="100%" height="100%" aria-hidden="false" tabindex="0"></iframe>
                     </div>
+{{--                    <div class="container">--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-lg-12">--}}
+{{--                                <div class="ttm_contact_widget_wrapper clearfix">--}}
+{{--                                    <h3>Opening Hours</h3>--}}
+{{--                                    <ul>--}}
+{{--                                        <li><h6>Monday</h6>12:00pm – 19:00 pm</li>--}}
+{{--                                        <li><h6>Tuesday To Friday</h6>12:00pm – 19:00 pm</li>--}}
+{{--                                        <li><h6>Saturday</h6>12:00pm – 19:00 pm</li>--}}
+{{--                                    </ul>--}}
+{{--                                    <a class="ttm-btn ttm-btn-size-md ttm-btn-shape-rounded ttm-btn-style-fill ttm-btn-color-skincolor margin_top10" href="contact-us.html">Make A Appoinment</a>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
             </div>
-        </section>
-        <!--Contact Page Google Map End-->
+
+        </div><!--site-main end-->
         @endif
 
 
