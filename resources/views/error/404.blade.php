3802567f600465da6ca3004270fa30c2b71caba5
anmolko <anmol@canosoft.com.np>
2022-12-26 16:28:49 +0545

    404 page update

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,42 +1,23 @@
 @extends('frontend.layouts.master')
 @section('title')  Page Not Found @endsection
 @section('content')
-        
 
-        <!--Page Header Start-->
-        <section class="page-header" style="background-image: url({{asset('assets/frontend/images/backgrounds/page-header-bg.jpg')}});">
-            <div class="page-header-shape-1"></div>
-            <div class="page-header-shape-2"></div>
+
+    <!--site-main start-->
+    <div class="site-main">
+
+        <section class="error-404 bg-img3">
             <div class="container">
-                <div class="page-header__inner">
-                    <ul class="thm-breadcrumb list-unstyled">
-                        <li><a href="/">Home</a></li>
-                        <li><span>.</span></li>
-                        <li>404 Error</li>
-                    </ul>
-                    <h2>404 Error</h2>
+                <div class="ttm-big-icon ttm-textcolor-skincolor">
+                    <i class="fa fa-exclamation-circle"></i>
                 </div>
+                <header class="page-header"><h1 class="page-title">404 ERROR</h1></header>
+                <div class="page-content"> <p>This page may have been moved or deleted. Be sure to check your spelling.</p></div>
+                <a class="ttm-btn ttm-btn-size-md ttm-btn-shape-rounded ttm-btn-style-border ttm-btn-color-dark" href="/">Back To Home</a>
             </div>
         </section>
-        <!--Page Header End-->
 
-        <!--Error Page Start-->
-        <section class="error-page">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="error-page__inner">
-                            <h2 class="error-page__title">404</h2>
-                            <h3 class="error-page__tagline">Sorry We Can't Find That Page!</h3>
-                            <p class="error-page__text">The page you are looking for was moved, removed, renamed or
-                                never existed.</p>
-                            <a href="/" class="thm-btn error-page__btn"><span>Back to home</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--Error Page End-->
+    </div><!--site-main end-->
 
 
 @endsection
